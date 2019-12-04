7f90fcc34a3e41e22a2334df1de1e84f17444d6d
inesmarca <imarcarian@itba.edu.ar>
2019-12-04 12:26:25 -0300

    Menu

--- candyTpe/src/game/frontend/GameApp.java
+++ candyTpe/src/game/frontend/GameApp.java
@@ -3,8 +3,13 @@ package game.frontend;
 import game.backend.CandyGame;
 import game.backend.level.Level1;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class GameApp extends Application {
 
@@ -12,14 +17,44 @@ public class GameApp extends Application {
 		launch(args);
 	}
 
-	@Override
-	public void start(Stage primaryStage) {
-		CandyGame game = new CandyGame(Level1.class);
+	public void start(Stage primaryStage) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+		primaryStage.setTitle("Candy Crush");
+		primaryStage.setResizable(false);
+		primaryStage.setScene(new Scene(root, 500, 600));
+		primaryStage.show();
+
+		/**
+		 CandyGame game = new CandyGame(Level1.class);
+		 CandyFrame frame = new CandyFrame(game);
+		 Scene scene = new Scene(frame);
+		 primaryStage.setResizable(false);
+		 primaryStage.setScene(scene);
+		 primaryStage.show();
+		 */
+
+
+	}
+
+
+	public void Level1(ActionEvent actionEvent) {
+		selectLevel(Level1.class);
+	}
+
+	public void Level2(ActionEvent actionEvent) {
+	}
+
+	public void Level3(ActionEvent actionEvent) {
+	}
+
+	public void selectLevel(Class<?> level) {
+		Stage secondStage = new Stage();
+		CandyGame game = new CandyGame(level);
 		CandyFrame frame = new CandyFrame(game);
 		Scene scene = new Scene(frame);
-		primaryStage.setResizable(false);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		secondStage.setResizable(false);
+		secondStage.setScene(scene);
+		secondStage.show();
 	}
 
 }
